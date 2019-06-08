bac3f7f67cd384ad3a82e8e25f2fa0142c4a0dd2
Mario Adamo Jr <marioadamojr@gmail.com>
2019-06-08 15:32:00 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Mario Adamo Jr"
+  end
+
+  get '/hometown' do
+    "My hometown is Leominster, MA"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Gettysburg by Ratata"
+  end
+
 end
